6239c547dd6411331b94849bd3da134916698193
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-02-19 10:10:57 +0200

    fixed some bugs

--- js/script.js
+++ js/script.js
@@ -2,24 +2,32 @@
 
 // TASK 1
 
-const birthdayDate = new Date('1995-06-18');
-let result = birthdayDate.toLocaleDateString();
-console.log(result);
+// const birthdayDate = new Date('1995-06-18');
+// let result = birthdayDate.toLocaleDateString();
+// console.log(result);
+
+
+// TASK 1 V2
+
+// const birthdayDate = new Date('1995-06-18');
+// let result = `${birthdayDate.getDate()}.0${birthdayDate.getMonth() + 1}.${birthdayDate.getFullYear()}`;
+// console.log(result);
 
 
 // TASK 2
 
 const getDiffDays = (a, b) => {
-    let firstDate = Date.parse(a);
-    let lastDate = Date.parse(b);
-    if ( isNaN(firstDate) === true || isNaN(lastDate) === true ) {
+    let firstDate = new Date(a);
+    let lastDate = new Date(b);
+    const MILL_TO_DAYS = 86400000;
+    if ( firstDate === "Invalid date" || lastDate === "Invalid date" ) {
         console.error("Введите корректные даты!");
         return;
     } else if (lastDate <= firstDate ) {
         console.error("Первая дата более поздняя, чем вторая!");
-        return;
+        return;s
     } else {
-        return (lastDate - firstDate) / 1000 / 60 / 60 / 24;
+        return (lastDate - firstDate) / MILL_TO_DAYS;
     }
 }
 
@@ -32,37 +40,32 @@ console.log(getDiffDays('222222222', '2020-03-15'));
 
 // TASK 3
 
-const isWeekend = (a) => {
-    let date = new Date(a);
-    let weekday = date.getDay();
-    if ( weekday === 0 || weekday === 6) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// const isWeekend = (date) => {
+//     const day = new Date(date).getDay();
+//     return day === 0 || day === 6;
+// }
 
-console.log(isWeekend('2022-02-12')); 
-console.log(isWeekend('2022-02-13')); 
-console.log(isWeekend('2022-02-09')); 
+// console.log(isWeekend('2022-02-12')); 
+// console.log(isWeekend('2022-02-13')); 
+// console.log(isWeekend('2022-02-09')); 
 
 
 // TASK 4
 
-const person = {
-    fullName: 'Sherlock Holmes',
-    address: {
-        street: "Baker Street",
-        city: "London",
-        house: "221b"
-    }
-}
+// const person = {
+//     fullName: 'Sherlock Holmes',
+//     address: {
+//         street: "Baker Street",
+//         city: "London",
+//         house: "221b"
+//     }
+// }
 
-let jsonPerson = JSON.stringify(person);
-console.log(jsonPerson);
+// let jsonPerson = JSON.stringify(person);
+// console.log(jsonPerson);
 
-let user = JSON.parse(jsonPerson);
-console.log(user);
+// let user = JSON.parse(jsonPerson);
+// console.log(user);
 
-const {fullName, address: {street, city, house} } = user;
-console.log(fullName, street, city, house);
+// const {fullName, address: {street, city, house} } = user;
+// console.log(fullName, street, city, house);
